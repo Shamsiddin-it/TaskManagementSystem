793e90b3bf3b6671082be88afa02e8f991f390d0
Umar Mirzoev <umarmitzoev@gmail.com>
2026-03-25 13:47:03 +0500

    v3

---
+++ backend/WebAPI/Controller/BudgetController.cs
@@ -0,0 +1,38 @@
+[ApiController]
+[Route("api/budget")]
+[Authorize(Roles = "Employer")]
+public class BudgetController : ControllerBase
+{
+    private readonly IBudgetService _service;
+    public BudgetController(IBudgetService service) => _service = service;
+
+    [HttpGet("org")]
+    public async Task<IActionResult> GetOrgBudget()
+    {
+        var employerId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        var res = await _service.GetOrgBudgetAsync(employerId);
+        return StatusCode(res.StatusCode, res);
+    }
+
+    [HttpPut("org")]
+    public async Task<IActionResult> UpdateOrgBudget([FromBody] UpdateOrgBudgetDto dto)
+    {
+        var employerId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        var res = await _service.UpdateOrgBudgetAsync(employerId, dto);
+        return StatusCode(res.StatusCode, res);
+    }
+
+    [HttpPost("records")]
+    public async Task<IActionResult> AddRecord([FromBody] CreateBudgetRecordDto dto)
+    {
+        var res = await _service.AddBudgetRecordAsync(dto);
+        return StatusCode(res.StatusCode, res);
+    }
+
+    [HttpGet("projects/{projectId:guid}")]
+    public async Task<IActionResult> GetProjectHistory(Guid projectId)
+    {
+        var res = await _service.GetProjectBudgetHistoryAsync(projectId);
+        return StatusCode(res.StatusCode, res);
+    }
+}
